9da0ff70f4afa02b6cb0f057f080ece371770453
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-17 16:53:30 +0800

    Initial commit

---
+++ AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Liko Setiawan on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
